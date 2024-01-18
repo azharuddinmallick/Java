5dd94bdc9036a6ca61ada5e564731612178f8753
Azhar Uddin Mallick <azharuddinmallick10@gmail.com>
2024-01-18 20:38:30 +0530

    Basic Skydiving Question

---
+++ Skydive.java
@@ -0,0 +1,9 @@
+public class Skydive {
+    public static void main(String args[]){
+        int age=55;
+        if(age>=18 && age<=60)
+        System.out.println("Yes, you can Skydive");
+        else
+        System.out.println("No, you can't Skydive");
+    }
+}
