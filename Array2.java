769f4b0a7aef40c40b9e62473b8c743301c1398f
Azhar Uddin Mallick <azharuddinmallick10@gmail.com>
2024-01-25 20:18:25 +0530

    Print sum of float values in an array

---
+++ Array2.java
@@ -0,0 +1,10 @@
+public class Array2 {
+    public static void main(String args[]){
+        float arr[]={1.1f,2.2f,3.3f,4.4f,5.5f};
+        float c=0f;
+        for(int i=0;i<arr.length;i++){
+            c+=arr[i];
+        }
+        System.out.println(c);
+    }
+}
