760316d0c717f78867d1a199efa15e346d68398b
Azhar Uddin Mallick <azharuddinmallick10@gmail.com>
2024-03-22 20:44:56 +0530

    Without Ten

--- WithoutTen.java
+++ WithoutTen.java
@@ -1,32 +1,40 @@
-public class WithoutTen {
+public class WithoutTenExample {
 
     public static void main(String[] args) {
         int[] nums1 = {1, 10, 10, 2};
         withoutTen(nums1);
-        printArray(nums1); 
+        printArray(nums1);
 
         int[] nums2 = {10, 2, 10};
         withoutTen(nums2);
-        printArray(nums2); 
+        printArray(nums2);
 
         int[] nums3 = {1, 99, 10};
         withoutTen(nums3);
-        printArray(nums3);  
+        printArray(nums3);
     }
 
     public static void withoutTen(int[] nums) {
         int nextNonTen = 0;
 
         for (int num : nums) {
-            if (num != 10) {
-                nums[nextNonTen] = num;
-                nextNonTen++;
+            try {
+                if (num != 10) {
+                    nums[nextNonTen] = num;
+                    nextNonTen++;
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {
+                break;
             }
         }
 
         while (nextNonTen < nums.length) {
-            nums[nextNonTen] = 0;
-            nextNonTen++;
+            try {
+                nums[nextNonTen] = 0;
+                nextNonTen++;
+            } catch (ArrayIndexOutOfBoundsException e) {
+                break;
+            }
         }
     }
 
