0f95e801ecd4db96a307eac96fb6ceb8eef5cae0
Azhar Uddin Mallick <azharuddinmallick10@gmail.com>
2024-03-28 20:54:43 +0530

    ArrayList

--- ArrayListSwap.java
+++ ArrayListSwap.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class ArrayList {
+public class ArrayListSwap {
     public static void main(String[] args) {
         ArrayList<String> arr = new ArrayList<>();
         arr.add("Item 1");
